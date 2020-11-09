cee7eddf07906910a91d7ac827966acbfdd37707
MartinPodhora <mato.podhora@gmail.com>
2020-11-09 21:57:31 +0100

    add dao and service for users

---
+++ src/main/java/com/myapp/dao/TeUserPersistence.java
@@ -0,0 +1,73 @@
+package com.myapp.dao;
+
+import com.myapp.TEException.TEException;
+import com.myapp.model.TeUser;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class TeUserPersistence {
+    @PersistenceContext(unitName = "")
+    private EntityManager entityManager;
+
+    public List<TeUser> retrieveAllUsers() throws TEException {
+        CriteriaBuilder cb = this.entityManager.getCriteriaBuilder();
+        CriteriaQuery<TeUser> query = cb.createQuery(TeUser.class);
+        Root<TeUser> root = query.from(TeUser.class);
+
+        query.select(root);
+
+        List<TeUser> users = this.entityManager.createQuery(query).getResultList();
+
+        if (users.isEmpty()) {
+            throw new TEException("There are no users in application.");
+        }
+
+        return users;
+    }
+
+    public TeUser findUser(String username) throws TEException {
+        TeUser user = this.entityManager.find(TeUser.class, username);
+
+        if (user == null) {
+            throw new TEException("User with specified username does not exists");
+        }
+
+        return user;
+    }
+
+    public TeUser persistUser(TeUser paUser) throws TEException {
+        TeUser user = this.entityManager.find(TeUser.class, paUser.getUsername());
+
+        if (user != null) {
+            throw new TEException("User with specified username already exists.");
+        }
+
+        this.entityManager.persist(paUser);
+        return paUser;
+    }
+
+    public TeUser updateUser(TeUser paUser) throws TEException {
+        TeUser user = this.findUser(paUser.getUsername());
+        this.entityManager.merge(user);
+        return paUser;
+    }
+
+    public String deleteUser(String username) throws TEException {
+        TeUser user = this.findUser(username);
+        this.entityManager.remove(user);
+        return username;
+    }
+
+    public TeUser logIn(TeUser paUser) throws TEException {
+        TeUser user = this.findUser(paUser.getUsername());
+        if (!user.getPassword().equals(paUser.getPassword())) {
+            throw new TEException("Wrong password.");
+        }
+        return user;
+    }
+}
---
+++ src/main/java/com/myapp/service/TeUserService.java
@@ -0,0 +1,71 @@
+package com.myapp.service;
+
+
+import com.myapp.TEException.TEException;
+import com.myapp.dao.TeUserPersistence;
+import com.myapp.model.TeUser;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Stateless
+@Path("/TeUser")
+@Tag(name = "Text editor users data", description = "Operations related to text editor users management")
+@Consumes(MediaType.APPLICATION_JSON)
+public class TeUserService {
+
+    @Inject
+    private TeUserPersistence dao;
+
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Get list of all users in Text editor")
+    public List<TeUser> getAllUsers() throws TEException {
+        return dao.retrieveAllUsers();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Get a user by username")
+    public TeUser getUser(@PathParam("username") String username) throws TEException {
+        return dao.findUser(username);
+    }
+
+    @POST
+    @Path("/create")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Create a new text editor user")
+    public TeUser createUser(TeUser user) throws TEException {
+        return dao.persistUser(user);
+    }
+
+    @PUT
+    @Path("/update")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Update a text editor user")
+    public TeUser updateUser(TeUser user) throws TEException {
+        return dao.updateUser(user);
+    }
+
+    @DELETE
+    @Path("/delete/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Delete text editor user")
+    public String deleteUser(@PathParam("username") String username) throws TEException {
+        return dao.deleteUser(username);
+    }
+}
